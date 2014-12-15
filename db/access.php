210bd70eb751f5fa5d32fdf07b165d5aa54ff1c1
Moirae <is.moirae@gmail.com>
2014-12-15 16:00:24 +0100

    Permissions and settings bug
    Wrong permissions setup disabled some users from logging into Moodle.
    Enabled settings in block.

--- db/access.php
+++ db/access.php
@@ -1,38 +1,47 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Capability definitions for the course_menu block
+ *
+ * @package    	block_helpdesk
+ * @author 		Ivana Skelic, Hrvoje Golcic
+ * @copyright	2014 IPA "Let's Study Together!" project
+ * @license    	http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-	'block/helpdesk:myaddinstance' => array(
-		'captype' => 'write',
-		'contextlevel' => CONTEXT_SYSTEM,
-		'archetypes' => array(
-			'user' => CAP_ALLOW
-		),
-
-		'clonepermissionfrom' => 'moodle/my:manageblocks'
-
-	),
-
-	'block/helpdesk:addinstance' => array(
-		'risk_bitmask' => RISK_SPAM | RISK_XSS,
-
-		'captype' => 'write',
-		'contextlevel' => CONTEXT_BLOCK,
-		'archetypes' => array(
-			'student' => CAP_ALLOW
-		),
-
-		'clonepermissionfrom' => 'moodle/site:manageblocks'
-	),
-
-	'block/helpdesk:cansend' => array(
-		'risk_bitmask' => RISK_SPAM | RISK_XSS,
-
-		'captype' => 'write',
-		'contextlevel' => CONTEXT_BLOCK,
-		'archetypes' => array(
-			'student' => CAP_ALLOW
-		),
-
-		'clonepermissionfrom' => 'moodle/site:manageblocks'
-	),
+    // New standard capability 'addinstance'.
+    'block/accessibility:addinstance' => array(
+        'captype'       => 'write',
+        'contextlevel'  => CONTEXT_COURSE,
+        'archetypes'    => array(
+            'editingteacher'    => CAP_ALLOW,
+            'manager'           => CAP_ALLOW
+        ),
+        'clonepermissionsfrom'  => 'moodle/site:manageblocks'
+    ),
+    'block/accessibility:myaddinstance' => array(
+      'riskbitmask'  => RISK_PERSONAL,
+      'captype'      => 'write',
+      'contextlevel' => CONTEXT_SYSTEM,
+      'archetypes'   => array(
+        'user' 		=> CAP_ALLOW,
+        'student'	=> CAP_ALLOW
+      ),
+      'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
 );
